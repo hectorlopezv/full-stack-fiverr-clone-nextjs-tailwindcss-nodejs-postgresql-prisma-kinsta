2190aa27fe4b90c03f1c61a28184cbd1979a0e6e
hector lopez <hectorvmlopez@gmail.com>
2023-06-27 18:13:43 -0500

    adding redux store

---
+++ app/store/store.ts
@@ -0,0 +1,40 @@
+import { devtools } from 'zustand/middleware';
+import { create, StateCreator } from 'zustand';
+
+interface BearSlice {
+  bears: number;
+  addBear: () => void;
+  eatFish: () => void;
+}
+const createBearSlice: StateCreator<
+  BearSlice & FishSlice,
+  [],
+  [],
+  BearSlice
+> = set => ({
+  bears: 0,
+  addBear: () => set(state => ({ bears: state.bears + 1 })),
+  eatFish: () => set(state => ({ fishes: state.fishes - 1 }))
+});
+
+interface FishSlice {
+  fishes: number;
+  addFish: () => void;
+}
+const createFishSlice: StateCreator<
+  BearSlice & FishSlice,
+  [],
+  [],
+  FishSlice
+> = set => ({
+  fishes: 0,
+  addFish: () => set(state => ({ fishes: state.fishes + 1 }))
+});
+
+const useBoundStore = create<BearSlice & FishSlice>()(
+  devtools((...a) => ({
+    ...createBearSlice(...a),
+    ...createFishSlice(...a)
+  }))
+);
+export { useBoundStore };
